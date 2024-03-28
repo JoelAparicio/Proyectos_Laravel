7ac668dc40d78e81b56c8834b9642851a1698150
JoelAparicio <105009975+JoelAparicio@users.noreply.github.com>
2024-03-28 03:11:32 -0500

    Proyecto Gestion academica 1.0

---
+++ Readme.txt
@@ -0,0 +1,7 @@
+//Primer paso en academicobackend
+Ejecutar php artisan migrate --seed
+
+//Segundo paso en academico-vue
+Ejecutar npm run serve
+
+//Todo corre a nivel local
---
+++ academico-vue/src/views/Inicio.vue
@@ -0,0 +1,23 @@
+<template>
+    <div id="carouselExample" class="carousel slide container-fluid">
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+            <img src="https://impulso06.com/wp-content/uploads/2023/09/Tecnicas-de-estudio-Efectivas-para-el-Exito-Academico-cual-usas-tu.png" class="d-block w-100 m-4 img-fluid">
+            </div>
+            <div class="carousel-item">
+            <img src="https://observatorio.tec.mx/wp-content/uploads/2022/05/Recomendacionesparaescribirunarti%CC%81culoacade%CC%81mico.jpeg" class="d-block w-100 img-fluid">
+            </div>
+            <div class="carousel-item">
+            <img src="https://www.ucatolica.edu.co/portal/wp-content/uploads/2022/11/FotoNoticia-1.jpg" class="d-block w-100 img-fluid" >
+            </div>
+        </div>
+        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Previous</span>
+        </button>
+        <button class="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Next</span>
+        </button>
+    </div>
+</template>
